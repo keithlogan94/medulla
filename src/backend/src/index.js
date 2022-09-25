74789a7d4a82d04af14b431c891f5bcd0ccdbc06
Keith Becker <keith.becker@recro.com>
2022-09-25 11:07:36 -0600

    npm

--- src/backend/src/index.js
+++ src/backend/src/index.js
@@ -3,18 +3,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', function (req, res) {
-    res.send('Platform')
-})
+app.get('/', function(req, res) {
+  res.send('Platform');
+});
 
-app.listen(3000)
-
-
-
+app.listen(3000);
 
 
