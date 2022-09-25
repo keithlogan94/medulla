39d4a607f2e28dc9ce41e6e6d6c1e2cf6085a721
Keith Becker <keith.becker@recro.com>
2022-09-25 11:19:58 -0600

    npm

--- src/backend/src/sequelize/sequelize.js
+++ src/backend/src/sequelize/sequelize.js
@@ -1,11 +1,27 @@
 import {Sequelize} from 'sequelize';
 
-const { DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST } = process.env;
+const {DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD,
+  DATABASE_HOST} = process.env;
 
-const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
-  host: DATABASE_HOST,
-  dialect: 'mysql',
-});
+/**
+ * Gets sequelize instance.
+ * @return {Sequelize} Sequelize instance to database connection.
+ */
+async function getSequelize() {
+  const sequelize = new Sequelize(DATABASE_NAME,
+      DATABASE_USERNAME, DATABASE_PASSWORD, {
+        host: DATABASE_HOST,
+        dialect: 'mysql',
+      });
 
 
-export default sequelize;
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+
+export default getSequelize();
